78abfb678e3e8f15e180d276934239ba4c78d07b
karol-bisztyga <karol.bisztyga@swmansion.com>
2020-07-23 14:24:03 +0200

    add animated reaction test, hooks note

--- src/reanimated2/Hooks.js
+++ src/reanimated2/Hooks.js
@@ -410,6 +410,12 @@ export function useAnimatedScrollHandler(handlers) {
   }, subscribeForEvents);
 }
 
+/**
+ * @param prepare - worklet used for data preparation for the second parameter
+ * @param react - worklet which takes data prepared by the one in the first parameter and performs certain actions
+ * the first worklet defines the inputs, in other words on which shared values change will it be called.
+ * the second one can modify any shared values but those which are mentioned in the first worklet. Beware of that, because this may result in endless loop and high cpu usage.
+ */
 export function useAnimatedReaction(prepare, react) {
   const inputsRef = useRef(null);
   if (inputsRef.current === null) {
